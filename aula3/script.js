2a382744fd6d8934dc6d4b0e6e2ea276a79008a3
Raphael Brandão <113994733+rbaa02@users.noreply.github.com>
2022-09-21 08:23:46 -0300

    Update script.js

--- aula3/script.js
+++ aula3/script.js
@@ -4,38 +4,54 @@ let tamanho = 2
 for (let i = 1; i < 7; i++) {
   titulo = document.querySelector(`h${i}`)
   if (titulo != null) {
-    botao()
+    inputRange()
     break
   }
 }
 
-function botao() {
-  const botaoMais = document.createElement(null)
-  botaoMais.innerHTML = `<button type='button' onclick="clicou('mais')" id='botao'>+</button>`
-
-  const botaoMenos = document.createElement(null)
-  botaoMenos.textContent = "-"
-  botaoMenos.innerHTML = `<button type='button' onclick="clicou('menos')" id='botao'>-</button>`
+function inputRange() {
+  const rangeI = document.createElement("div")
+  rangeI.classList.add("box")
+  rangeI.innerHTML = "<div class='input'><input type='range' min='1' max='8' value='5'></input><div class='value'>5</div></div>"
+  
 
   const br = document.createElement("br")
 
   titulo.parentNode.insertBefore(br, titulo.nextSibling)
-  titulo.parentNode.insertBefore(botaoMenos, titulo.nextSibling)
-  titulo.parentNode.insertBefore(botaoMais, titulo.nextSibling)
+  titulo.parentNode.insertBefore(rangeI, titulo.nextSibling)
+  rangeInput()
+  rangeCss()
+}
+
+function rangeInput() {
+  const rangeInput = document.querySelector("input")
+  const value = document.querySelector(".value")
   
-  const botao = document.querySelectorAll('#botao')
-  for(let i = 0; i < 2; i++){
-    botao[i].style.borderRadius = '10px'
-    botao[i].style.width = '30px'
-    botao[1].style.marginLeft = '20px'
+  rangeInput.oninput = ()=>{
+    let rangeI = rangeInput.value
+    value.innerText = rangeI
+    titulo.style.fontSize = `${rangeI}em`
   }
 }
 
-function clicou(btn) {
-  if (btn == "mais" && tamanho < 6) {
-    tamanho += 0.5
-  } else if(btn == 'menos' && tamanho > 1) {
-    tamanho -= 0.5
-  }
-  titulo.style.fontSize = `${tamanho}em`
+function rangeCss(){
+  const box = document.querySelector(".box")
+  const input = document.querySelector(".input")
+  const inputR = document.querySelector("input")
+  const value = document.querySelector(".value")
+  box.style.display = "flex"
+  box.style.alingItems = "center"
+  box.style.margin = "0 auto"
+  box.style.backgroundColor = "white"
+  box.style.padding = "5px"
+  box.style.borderRadius = "5px"
+  box.style.width = "250px"
+  
+  input.style.display = "flex"
+  input.style.alingItems = "center"
+  input.style.margin = "0 auto"
+  
+  inputR.style.width = "200px"
+  
+  value.style.paddingLeft = "5px"
 }
