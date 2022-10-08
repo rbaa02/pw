bf175411b838fecaec6bc5f654e75559be89dff6
Raphael Brandão <113994733+rbaa02@users.noreply.github.com>
2022-10-08 15:14:37 -0300

    Create scritp.js

---
+++ aula5/scritp.js
@@ -0,0 +1,35 @@
+let inputValue;
+const ul = document.querySelector("ul");
+const h1 = document.querySelector("h1");
+
+const eventL = () => {
+  document.addEventListener("change", (e) => {
+    inputValue = e.target.value;
+    main();
+    h1.innerText = "Cidades com o DDD " + e.target.value;
+    e.target.value = "";
+  });
+};
+
+const fetchDDD = async (ddd) => {
+  const fetchResponse = await fetch(
+    `https://brasilapi.com.br/api/ddd/v1/${ddd}`
+  );
+  return await fetchResponse.json();
+};
+
+const setResponse = (name) => {
+  const li = document.createElement("li");
+  li.innerText = name;
+  ul.appendChild(li);
+};
+
+const main = async () => {
+  ul.innerHTML = "";
+  h1.innerText = "Cidades com o DDD";
+  const data = await fetchDDD(inputValue);
+  const cities = data.cities ? data.cities : 0;
+  for (let i = 0; i < cities.length; i++) {
+    setResponse(cities[i]);
+  }
+};
