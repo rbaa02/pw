817fc500ead2a2f2083ff412689777673d83f2e5
Raphael Brandão <113994733+rbaa02@users.noreply.github.com>
2022-10-07 23:26:10 -0300

    Update script.js

--- aula4/script.js
+++ aula4/script.js
@@ -1,145 +1,127 @@
-const tamanhoCelula = 40;
-let pecaId = 0;
-let localAtual = 80;
-let localFuturo = 81;
-let classe ='';
-let localCaptura = '';
-let jogada = 0;
-document.body.append(criaTabuleiro());
+const Damas = {
+    pecaId: 0,
+    idPeca: 0,
+    jogada: 0,
+  
+    posAtual: '',
+    pecaAtual: '',
+  
+    posCaptura: '',
+    pecaCapturada: '',
+    nomePecaCapturada: '',
 
-function criaTabuleiro() {
-    const tamanho = 8;
-    let tabela = document.createElement('table');
+     criaTabuleiro: function(){
+        let tamanhoTablueiro = 8
 
-    tabela.style.borderStyle = 'solid';
-    tabela.style.borderSpacing = 0;
-    tabela.style.margin = 'auto';
+        let tabela = document.createElement('table')
+        for(let l = 0; l < tamanhoTablueiro; l++){
+            const linha = document.createElement('tr')
 
-    for (let i = 0; i < tamanho; i++) {
-        let linha = document.createElement('tr');
-        tabela.append(linha);
-        for (let j = 0; j < tamanho; j++) {
-            let celula = document.createElement('td');
-			celula.setAttribute('id',`${j}` + '-' + `${i}`);
-            linha.append(celula);
-            celula.style.width = `${tamanhoCelula}px`;
-            celula.style.height = `${tamanhoCelula}px`;
-			pecaId += 1;
-	
-            if (i % 2 == j % 2) {
-                celula.style.backgroundColor = 'black';
-				celula.setAttribute("class","droptarget");
-                if (i * 8 + j <= 24) {
-                    celula.append(criaPeca('black',pecaId));
-                } else if (i * 8 + j >= 40) {
-                    celula.append(criaPeca('red',pecaId));
+            for(let c = 0; c < tamanhoTablueiro; c++){
+                const casa = document.createElement('td')
+
+                casa.dataset.linha = l
+                casa.dataset.coluna = c
+              
+                casa.setAttribute('id', `${l}-${c}`)
+
+                if (l % 2 == c % 2) {
+                    casa.style.backgroundColor = 'white'
+                    
+                } else {
+                    casa.style.backgroundColor = 'black'
+                    casa.classList.add('alvo')
+                  
+                    if (l * 8 + c < 24) {
+                          casa.append(this.criaPeca('black'))
+                          
+                      } else if (l * 8 + c >= 40) {
+                          casa.append(this.criaPeca('red'))
+                      }
                 }
-            } else {
-                celula.style.backgroundColor = 'white';
+                linha.appendChild(casa)
             }
+            tabela.appendChild(linha)
         }
-    };
-	
-    return tabela;	
-}
+        document.body.appendChild(tabela)
+    },
 
-function criaPeca(cor,ide) {
-		let imagem = document.createElement('img');
-		imagem.setAttribute('src', `img/${cor}.png`);
-		imagem.setAttribute('width', `${tamanhoCelula-4}px`);
-		imagem.setAttribute('height', `${tamanhoCelula-4}px`);
-		imagem.setAttribute('draggable','true');
-		imagem.setAttribute('id', ide);
-		imagem.setAttribute('class', cor);
-		
-    return imagem;
-}
+    start: function(){
+      document.addEventListener('dragstart', (e)=>{
+      this.posAtual = (e.target.parentElement.id)
+      this.idPeca = parseInt(e.target.id)
+      this.pecaAtual = (e.target.className)
+      })
+    },
 
-function dragstart(){
-	document.addEventListener("dragstart", function(event) {
-	  event.dataTransfer.setData("Text", event.target.id);
-	  localAtual = event.path[1].id.toString();
-	  classe = event.path[0].className;
-	});
-}
+    over: function(){
+      document.addEventListener('dragover', function(e){
+        e.preventDefault()
+      })
+    },
 
-function dragend() {
-	document.addEventListener("dragend", function(event) {
-	});
-}
+    drop: function(){
+      document.addEventListener('drop', (e)=>{
+        e.preventDefault()
+        console.log('entrou')
+        if(e.target.className == 'alvo'){ 
 
-function dragover() {
-	document.addEventListener("dragover", function(event) {
-	  event.preventDefault();
-	});
-}
+          const peca = document.getElementById(`${this.idPeca}`)
 
-function drop(){
-	document.addEventListener("drop", function(event) {
-	event.preventDefault();
-	if ( event.target.className == "droptarget") {
-		const data = event.dataTransfer.getData("Text");
-		let c = event.path[0];
-		let t = c.childElementCount;
-		localFuturo = event.target.id.toString();
-		let xa = localAtual.substring(0,1);
-		let ya = localAtual.substring(2,3);
-		let xf = localFuturo.substring(0,1);
-		let yf = localFuturo.substring(2,3);
-		
-		if(classe == 'black' && xf < xa) {
-			localCaptura = (parseInt(xa) - 1).toString() + "-" + (parseInt(ya) + 1).toString();
-		} else if(classe == 'black' && xf > xa) {
-			localCaptura = (parseInt(xa) + 1).toString() + "-" + (parseInt(ya) + 1).toString();
-		} else if(classe == 'red' && xf > xa){
-			localCaptura = (parseInt(xa) + 1).toString() + "-" + (parseInt(ya) - 1).toString();
-		} else if(classe == 'red' && xf < xa){
-			localCaptura = (parseInt(xa) - 1).toString() + "-" + (parseInt(ya) - 1).toString();
-		}
-		captura = document.getElementById(localCaptura);
-		if(captura.childElementCount == '1'){
-			classeCapturada = captura.firstElementChild.className;
-			pecaCapturada = captura.firstElementChild;
-		}
-		if (t == '0' && ya != yf){
-			if (classe == 'red' && ya > yf && ya - yf == 1 && jogada % 2 == 0 || ya - yf == 2 && classeCapturada == "black" && jogada % 2 == 0 || classe == 'black' && ya < yf && ya - yf == -1 && jogada % 2 == 1 || ya - yf == -2 && classeCapturada == "red" && jogada % 2 == 1) {
-				event.target.appendChild(document.getElementById(data));
-				if (classe == 'red' && yf == '0' || classe == 'black' && yf == '7'){
-					pecaRainha(classe,yf,xf);
-				}
-				jogada += 1;
-				if(ya - yf == 2 || ya - yf == -2) {
-					pecaCapturada.remove();
-					pecaCapturada = '';
-					lasseCapturada = '';
-				}
-			}
-		}
-		
-	}
-	});
+          const la = parseInt(peca.parentElement.dataset.linha)  //linha atual
+          const ca = parseInt(peca.parentElement.dataset.coluna) //coluna atual
+          const ld = parseInt(e.target.dataset.linha) //linha destino
+          const cd = parseInt(e.target.dataset.coluna) //coluna destino
+
+          
+          if(this.pecaAtual == 'black' && ca > cd){ // encontra a casa pra capturar (meio)
+              this.posCaptura = `${la + 1}-${ca - 1}`
+            
+          }else if(this.pecaAtual == 'black' && ca < cd){
+              this.posCaptura = `${la + 1}-${ca + 1}`
+            
+          }else if(this.pecaAtual == 'red' && ca > cd){
+              this.posCaptura = `${la - 1}-${ca - 1}`
+              
+          }else if(this.pecaAtual == 'red' && ca < cd){
+              this.posCaptura = `${la - 1}-${ca + 1}`
+          }
+        
+          //capturar (encontrar a peca e guardar o nome dela)
+          const alvo = (document.getElementById(this.posCaptura))
+          if(alvo.childElementCount == 1){
+            this.pecaCapturada = alvo.firstElementChild
+            this.nomePecaCapturada = alvo.firstElementChild.className
+          }
+          
+          if(la != ld || la == ld){ // verifica se a linha é diferente pra não pular linha
+
+            //verifica as jogadas e se ouve alguma peça capturada
+            if(this.pecaAtual == 'red' && la > ld && la - ld == 1 && this.jogada % 2 == 0 || 
+              la - ld == 2 && this.nomePecaCapturada == 'black' && this.jogada % 2 == 0 ||
+               this.pecaAtual == 'black' && la < ld && la - ld == -1 && this.jogada % 2 == 1 ||
+               la - ld == -2 && this.nomePecaCapturada == 'red' && this.jogada % 2 == 1
+              ){
+                e.target.appendChild(peca)
+                this.jogada ++
+              
+                if(la - ld == 2 || la - ld == -2){ 
+                    this.pecaCapturada.remove()
+                }
+            }
+          }
+        }
+      })
+    },
+
+    criaPeca: function(cor){
+        let imagem = document.createElement('img')
+        imagem.setAttribute('src', `img/${cor}.png`)
+        imagem.classList.add(cor)
+        imagem.id = `${this.pecaId++}`
+        return imagem
+    }
 }
-function pecaRainha(classe, yf, xf){
-	local = xf + '-' + yf;
-	sub = document.getElementById(local);
-	pe = sub.firstElementChild;
-	addId = pe.id;
-	addClass = pe.className;
-	let imagem = document.createElement('img');
-	if (classe == "red") {
-		imagem.setAttribute('src', 'img/redKing.jpeg');
-	} else {
-		imagem.setAttribute('src', 'img/blackKing.jpeg');
-	}
-	imagem.setAttribute('width', `${tamanhoCelula-4}px`);
-	imagem.setAttribute('height', `${tamanhoCelula-4}px`);
-	imagem.setAttribute('draggable','true');
-	imagem.setAttribute('id', addId);
-	imagem.setAttribute('class', `${addClass}King`);
-	pe.remove();
-	sub.append(imagem);
-}
-dragstart();
-dragend();
-dragover();
-drop();
+Damas.over()
+Damas.drop()
+Damas.start()
