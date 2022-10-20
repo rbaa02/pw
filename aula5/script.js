9fa967b3e7cd7b408a9df0c061d6e9012706e523
Raphael Brandão <113994733+rbaa02@users.noreply.github.com>
2022-10-20 23:46:24 +0000

    aula5

--- aula5/script.js
+++ aula5/script.js
@@ -1,34 +1,32 @@
 const ul = document.querySelector("ul");
-const h1 = document.querySelector("h1");
-
-const eventL = () => {
-  document.addEventListener("change", (e) => {
-    inputValue = e.target.value;
-    main();
-    h1.innerText = "Cidades com o DDD " + e.target.value;
-    e.target.value = "";
-  });
-};
-
-const fetchDDD = async (ddd) => {
-  const fetchResponse = await fetch(
-    `https://brasilapi.com.br/api/ddd/v1/${ddd}`
-  );
-  return await fetchResponse.json();
-};
+let data = new Array();
 
 const setResponse = (name) => {
-  const li = document.createElement("li");
-  li.innerText = name;
-  ul.appendChild(li);
+    const li = document.createElement("li");
+    li.innerText = name;
+    ul.appendChild(li);
 };
 
-const main = async () => {
-  ul.innerHTML = "";
-  h1.innerText = "Cidades com o DDD";
-  const data = await fetchDDD(inputValue);
-  const cities = data.cities ? data.cities : 0;
-  for (let i = 0; i < cities.length; i++) {
-    setResponse(cities[i]);
-  }
+const fetchAPI = (inputValue) => {
+    ul.innerHTML = "";
+    fetch(`https://brasilapi.com.br/api/ddd/v1/${inputValue}`)
+    .then(response => response.json())
+    .then(dados => {
+        if (dados.cities) {
+            dados.cities.forEach(e => {
+                setResponse(e);
+            })
+        }
+    });
 };
+
+const h1 = (e) => {
+    const h1 = document.querySelector("h1");
+    h1.innerText = "Cidades com o DDD " + e;
+};
+
+document.getElementById('ddd').addEventListener("change", (e) => {
+    fetchAPI(e.target.value);
+    h1(e.target.value)
+    e.target.value = "";
+});
