2584aa76785e49722ca595a7e2c77e1777b5e43d
Raphael Brandão <113994733+rbaa02@users.noreply.github.com>
2022-10-21 10:00:08 -0300

    Update script.js

--- aula5/script.js
+++ aula5/script.js
@@ -2,31 +2,35 @@ const ul = document.querySelector("ul");
 let data = new Array();
 
 const setResponse = (name) => {
-    const li = document.createElement("li");
-    li.innerText = name;
-    ul.appendChild(li);
+   const li = document.createElement("li");
+   li.innerText = name;
+   ul.appendChild(li);
 };
 
 const fetchAPI = (inputValue) => {
-    ul.innerHTML = "";
-    fetch(`https://brasilapi.com.br/api/ddd/v1/${inputValue}`)
-    .then(response => response.json())
-    .then(dados => {
-        if (dados.cities) {
+   ul.innerHTML = "";
+   fetch(`https://brasilapi.com.br/api/ddd/v1/${inputValue}`)
+      .then(response => response.json())
+      .then(dados => {
+         if (dados.cities) {
             dados.cities.forEach(e => {
-                setResponse(e);
+               setResponse(e);
             })
-        }
-    });
+         }
+      });
 };
 
 const h1 = (e) => {
-    const h1 = document.querySelector("h1");
-    h1.innerText = "Cidades com o DDD " + e;
+   const h1 = document.querySelector("h1");
+   h1.innerText = "Cidades com o DDD " + e;
 };
 
 document.getElementById('ddd').addEventListener("change", (e) => {
-    fetchAPI(e.target.value);
-    h1(e.target.value)
-    e.target.value = "";
+   e.preventDefault()
+   fetchAPI(e.target.value);
+   h1(e.target.value)
+   e.target.value = "";
 });
+
+document.addEventListener("submit", 
+(e) => {e.preventDefault()})
